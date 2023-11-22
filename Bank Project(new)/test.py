c9878d9d8207d4eb3f180e18bbeb917b8cd5862b
Hxrshmxllow <86836424+Hxrshmxllow@users.noreply.github.com>
2023-11-22 13:22:44 +0000

    yes

--- Bank Project(new)/test.py
+++
@@ -1,9 +0,0 @@
-from wbscraper import main
-#import subprocess
-#subprocess.call(['python', 'wbscraper.py', '3845811748', 'harshit@3126'])
-
-gpa = main("3845811748", "harshit@3126")
-print(round(gpa, 3))
-
-#gpa = wbscraper("3845811748", "harshit@3126")
-#print(gpa)
