e1e42f4edf15ebc168ba5a08821dd2313204c998
Hxrshmxllow <smartharshit126@gmail.com>
2023-11-26 17:19:19 -0500

    added html files

---
+++ js/nav.js
@@ -0,0 +1,12 @@
+const createNav = () => {
+    let nav = document.querySelector(".nav");
+
+    nav.innerHTML = `
+        <div class="nav">
+            <h1> Hxrshmxllow </h1>
+        </div>
+    `;
+}
+
+createNav();
+
